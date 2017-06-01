67403336dbf42ed7923ad276f553ce85d8d1c683
Christian Glahn <cpglahn@gmail.com>
2017-06-01 11:17:50 +0200

    integrate changes for version 2

--- provider/adapters/ldapmanager.js
+++ provider/adapters/ldapmanager.js
@@ -1,12 +1,16 @@
 "use strict";
 
 const LDAPConnection = require("./ldapconnection.js");
+const splitLabeledUri = require("../helper/splitLabeledUri");
 
 const connection = {};
+
+LDAPConnection.addAttributeHandler(splitLabeledUri);
 
 module.exports = async function findLdapConnection(name, settings) {
     if (!connection[name] && settings.directory[name]) {
         connection[name] = new LDAPConnection(settings.directory[name]);
+        await connection[name].connect();
     }
     return connection[name];
 };
