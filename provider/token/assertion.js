71cd6f7a63027cde2ad12e060cb95f1b888178fa
Christian Glahn <cpglahn@gmail.com>
2017-07-13 12:29:52 +0200

    follow the official release

--- provider/token/assertion.js
+++ provider/token/assertion.js
@@ -3,50 +3,20 @@
 /**
  * Implementation of a jwt bearer assertion
  */
-var jose = require("node-jose");
+const compose = require('koa-compose');
+const paramsMiddleware = require('node-oidc-provider/shared/get_params');
+
+const stack = require('../actions/assertion');
 
 const grant_type = "urn:ietf:params:oauth:grant-type:jwt-bearer";
 
-function assertionGrantTypeFactory(providerInstance) {
-    return function *assertionGrantType(next) {
-        // grab the user name and password
-        const assertion = this.oidc.params.assertion;
 
-        // we use a JWT
-        // TODO
-        // an assertion token is a JWE for the provider.
-        // it holds a JWS signed by a client that has a valid key in our
-        // data store.
-        const AssertionToken = providerInstance.AssertionToken;
+const getParams = paramsMiddleware(["grant_type", "assertion"]]);
 
-
-
-        if (this.oidc.params.username === "foo" && this.oidc.params.password === "bar") {
-            const AccessToken = providerInstance.AccessToken;
-            const at = new AccessToken({
-                accountId: "foo",
-                clientId: this.oidc.client.clientId,
-                grantId: this.oidc.uuid,
-            });
-
-            const accessToken = yield at.save();
-            const tokenType = "Bearer";
-            const expiresIn = AccessToken.expiresIn;
-
-            this.body = {
-                access_token: accessToken,
-                expires_in: expiresIn,
-                token_type: tokenType,
-            };
-        }
-        else {
-            this.body = {
-                error: "invalid_grant",
-                error_description: "invalid credentials provided",
-            };
-            this.status = 400;
-        }
-
-        yield next;
-    };
+function assertionGrantTypeFactory(provider) {
+    return compose([
+        getParams,
+        stack.verifyAssertion(provider),
+        stack.checkClient(provider),
+    ]);
 }
