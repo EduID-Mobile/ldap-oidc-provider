b7f0b3c2552fb81475260dde5325b2a5eb003de2
phish108 <>
2018-05-01 12:47:11 +0200

    wait for async connection drivers

--- provider/adapters/factory.js
+++ provider/adapters/factory.js
@@ -1,31 +1,51 @@
 "use strict";
 
 const Debug = require("debug");
-const debug = Debug("ldap-oidc:adapter:factory");
+const log = Debug("ldap-oidc:adapter:factory");
+const debug = Debug("dev-ldap-oidc:adapter:factory");
 
 const RedisAdapter   = require("oidc-redis-adapter");
 const MemoryAdapter  = require("./memory");
 const LdapAdapter    = require("oidc-ldap-adapter");
 
-module.exports = function AdapterFactory(sources, configuration) {
-  // connection cache
-    const Cache = new Map();
-    const CCache = new Map();
+const CCache = new Map();
 
-  // init data sources
+async function initAdapters(sources) {
+    let connection;
+
     for (let source in sources) {
         debug(`init ${source} with connection type ${sources[source].type}`);
         switch (sources[source].type) {
                 case "ldap":
-                    CCache.set(source, new LdapAdapter.connection(sources[source]));
+                    try {
+                        connection = await LdapAdapter.connection(sources[source]);
+                    }
+                    catch(err) {
+                        log(`ldap failure ${source}: ${err.message}`);
+                    }
                     break;
                 case "redis":
-                    CCache.set(source, new RedisAdapter.connection(sources[source]));
+                    try {
+                        connection = RedisAdapter.connection(sources[source]);
+                    }
+                    catch(err) {
+                        log(`redis failure ${source}: ${err.message}`);
+                    }
                     break;
                 default:
                     break;
         }
+        CCache.set(source, connection);
     }
+}
+
+module.exports = function AdapterFactory(sources, configuration) {
+  // connection cache
+    const Cache = new Map();
+
+  // init data sources
+
+    initAdapters(sources).then(() => {});
 
     return function getAdapter(name) {
         if (Cache.has(name)) {
@@ -36,16 +56,16 @@ module.exports = function AdapterFactory(sources, configuration) {
         configuration[name].source &&
         CCache.has(configuration[name].source)) {
 
-            debug(`init ${name}`);
+            log(`init ${name}`);
             const src = CCache.get(configuration[name].source);
             let adapter;
 
             switch (sources[configuration[name].source].type) {
                     case "ldap":
-                        adapter = new LdapAdapter(name, src, configuration[name].options);
+                        adapter = new LdapAdapter(name, src, configuration[name]);
                         break;
                     case "redis":
-                        adapter = new RedisAdapter(name, src, configuration[name].options);
+                        adapter = new RedisAdapter(name, src, configuration[name]);
                         break;
                     default:
                         adapter = new MemoryAdapter(name);
